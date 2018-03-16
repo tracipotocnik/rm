afd12c8508f1b42f713b2dfdfe6e655b0ea90178
Traci Potocnik <tracipotocnik@Tracis-MacBook-Pro.local>
2018-03-16 15:28:13 -0400

    used deploy prime url config option

--- config/test.env.js
+++ config/test.env.js
@@ -3,5 +3,5 @@ var devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
   NODE_ENV: '"testing"',
-  API_URL: '"https://api.routemarket.com:8443"'
+  API_URL: '"https://api-test.routemarket.com:8443"'
 })
