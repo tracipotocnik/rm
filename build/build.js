582b0a5951dc1ede38b7a314034fe15758323d66
Traci Potocnik <tracipotocnik@Tracis-MacBook-Pro.local>
2018-03-16 14:25:28 -0400

    using req instead of window

--- build/build.js
+++ build/build.js
@@ -1,11 +1,10 @@
 require('./check-versions')()
 
 var isBrowser = typeof window !== 'undefined';
-process.env.NODE_ENV = isBrowser && global.window.location.hostname === 'master--routemarket.netlify.com'
+process.env.NODE_ENV = req.get('host') === 'master--routemarket.netlify.com'
   ? 'testing'
   : 'production'
-console.log('is browser: ' + isBrowser);
-console.log(global.window.location.hostname);
+console.log(req.get('host'));
 console.log(process.env.NODE_ENV);
 
 var ora = require('ora')
