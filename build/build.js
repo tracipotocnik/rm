04a9d91f1cada3ffe0222b205f3c52f8fcfe2d25
Traci Potocnik <tracipotocnik@Tracis-MacBook-Pro.local>
2018-03-16 15:09:01 -0400

    Added netlify.toml

--- build/build.js
+++ build/build.js
@@ -1,9 +1,7 @@
 require('./check-versions')()
 
-process.env.NODE_ENV = $NODE_ENV
-  ? $NODE_ENV
-  : 'production'
-console.log('Node Environment!! ' + $NODE_ENV);
+console.log('Node Environment!! ' + process.env.NODE_ENV);
+process.env.NODE_ENV = 'production'
 
 var ora = require('ora')
 var rm = require('rimraf')
